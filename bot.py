45d10e7b4656908909ce064bb844d5f6a9e97608
lorex279 <zetafixeye777@gmail.com>
2025-10-02 21:46:23 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("С Днём учителя ❤️")
+
+def main():
+    token = os.environ.get("TELEGRAM_BOT_TOKEN")
+    if not token:
+        print("Ошибка: переменная окружения TELEGRAM_BOT_TOKEN не задана.")
+        return
+
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("hello", hello_command))
+    print("Бот запущен!")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
